88636bbc4584123e7e6226ecc86a78c55a03afc7
Roy Hu <roy@larvata.tw>
2023-02-16 22:34:40 +0800

    feat: Implement display for UIDisplayContext

--- CleanArcDesign/Routers/Router.swift
+++ CleanArcDesign/Routers/Router.swift
@@ -16,7 +16,7 @@ public class Router {
             displayContext.display(target)
             configuration?(source, target)
         case let .close(target):
-            target.displayContext?.undisplay()
+            target.displayContext?.undisplay(target)
         case .custom:
             break
         }
@@ -37,19 +37,93 @@ public enum RouteBehavior {
 }
 
 public struct UIDisplayContext {
+    
     public func display(_ viewController: UIViewController) {
+        switch method {
+        case let .present(animated, presentationStyle):
+            viewController.modalPresentationStyle = presentationStyle
+            sourceViewController?.present(viewController, animated: animated)
+        case let .push(animated):
+            if let navigationController = sourceViewController as? UINavigationController
+                ?? sourceViewController?.navigationController {
+                navigationController.pushViewController(viewController, animated: animated)
+            }
+        case let .embed(view):
+            guard let sourceViewController = sourceViewController else {
+                return
+            }
+            
+            let sourceView = view ?? sourceViewController.view
+            if let nc = sourceViewController as? UINavigationController,
+               sourceViewController.tabBarController != nil {
+                // Fix a problem of extra gap to bottom when embedding navigation controllors
+                nc.children.forEach { $0.extendedLayoutIncludesOpaqueBars = true }
+            }
+            
+            sourceViewController.embed(viewController, over: sourceView)
+            
+            
+        case .custom(_):
+            fatalError("Unimplemented for this demo")
+        }
         
+
     }
     
-    public func undisplay() {
-        
+    public func undisplay(_ viewController: UIViewController) {
+        switch method {
+        case .present(let animated, _):
+            if viewController.presentedViewController != nil {
+                viewController.dismiss(animated: false, completion: nil)
+            }
+            
+            
+            viewController.dismiss(animated: animated, completion: nil)
+            
+
+        case .push(let animated):
+            if viewController.presentedViewController != nil { // dismiss presented children first
+                viewController.dismiss(animated: false, completion: nil)
+            }
+
+            if let navigationController = sourceViewController as? UINavigationController
+                                                  ?? sourceViewController?.navigationController {
+                if navigationController.topViewController == viewController {
+                    navigationController.popViewController(animated: animated)
+                } else if let index = navigationController.viewControllers.firstIndex(of: viewController),
+                          index >= 1 {
+                    navigationController.popToViewController(
+                        navigationController.viewControllers[index-1],
+                        animated: animated)
+                }
+            }
+        case .embed(_):
+            if viewController.presentedViewController != nil { // dismiss presented children first
+                viewController.dismiss(animated: false, completion: nil)
+            }
+            
+            viewController.unembedFromParent()
+
+//            viewController.removeFromParent()
+//            viewController.view.removeFromSuperview()
+//            viewController.didMove(toParent: nil)
+//        case .appendToTabBarController(let source):
+//            guard let source = source as? UITabBarController else { break }
+//            var viewControllers = source.viewControllers ?? []
+//            if let index = viewControllers.firstIndex(of: source) {
+//                viewControllers.remove(at: index)
+//                source.viewControllers = viewControllers
+//            }
+        case .custom(_):
+            fatalError("Unimplemented for demo")
+        }
     }
     
     public weak var sourceViewController: UIViewController?
     public var method: DisplayMethod
     
     public enum DisplayMethod {
-        case present(animated: Bool)
+        case present(animated: Bool, presentationStyle: UIModalPresentationStyle = .fullScreen)
         case push(animated: Bool)
         case embed(over: UIView? = nil)
         case custom(userInfo: Any)
